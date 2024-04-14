1f6b52b16aa64d215aff76015df019e0611a5a08
ysh2024 <165976269+ysh2024@users.noreply.github.com>
2024-04-13 21:40:16 -0400

    Add files via upload

--- generateEMR.c
+++ generateEMR.c
@@ -1,3 +1,4 @@
+// This program generates EMR by frequently writing data to memory. The frequency of EMR is correlated with the frequency of writing data. 
 #include <sys/time.h>
 #include <math.h>
 #include <immintrin.h> //__m512i
@@ -27,66 +28,24 @@ int chip[128]={1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,1,3,3,3,3,3,3,3,1,4,4,4,4,4,4,4,1
 
 
 struct timeval curTime;
-long int thres;
-long int dur;
-long long start;
-long long finish;
 long long rdLen;
-long long sbDur[8];
+long long sbDur[8]; // used to specify the number of repetitions needed when transmitting different chips
 double SBDUR;
-double temp;
 long long dataLen=sizeof(data)/sizeof(data[0]);
 long long headLen=sizeof(head)/sizeof(head[0]);
-int waitus,sleepus;
+int waitus;
 long int startTime, endTime;
-
-void sigchld_handler(int s)
-{
-	(void)s; // quiet unused variable warning
-
-	// waitpid() might overwrite errno, so we save and restore it:
-	int saved_errno = errno;
-
-	while(waitpid(-1, NULL, WNOHANG) > 0);
-
-	errno = saved_errno;
-}
-
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
-
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-int server(void)
-{
-	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-	int cnt=1, fourClient[4];
-
-	while(1) {
-		if (cnt==1){
-			send(fourClient[0], "begin", 5, 0);
-			break;
-		}
-	}
-	return 0;
-}
 
 void WriteRAM(int array[], long long arrayLen){
 for (rdLen=0;rdLen<arrayLen;rdLen++) {
-		//printf("%d\n",a[rdLen]);
-
+		// To generate EMR at different frequencies when transmitting different chips, the frequency of repeated memory writes for each chip are different.
+		// The proportion of write operations to idle operations varies between different chips, consequently resulting in different EMR frequencies.
 		switch(array[rdLen]){
 
 			case 0:
 				for (val=1;val<SBDUR;val++) {
 				    asm volatile(			    
-					" mfence \n"
+					" mfence \n" // idle
 					" mfence \n"
 					" mfence \n"
 					" mfence \n"
@@ -105,22 +64,22 @@ for (rdLen=0;rdLen<arrayLen;rdLen++) {
 			case 1:
 				for (val=1;val<sbDur[0];val++) {
 				    asm volatile(			    
-					" clflush (%0) \n"//clear reg
-					" clflush (%1) \n"//clear reg
-					" clflush (%2) \n"//clear reg
-					" clflush (%3) \n"//clear reg
-					" clflush (%4) \n"//clear reg
-					" clflush (%5) \n"//clear reg
-					" clflush (%6) \n"//clear reg
-					" clflush (%7) \n"//clear reg
-					" vmovdqa %%ymm0, (%0) \n"//read from RAM
-					" vmovdqa %%ymm0, (%1) \n"//read from RAM
-					" vmovdqa %%ymm0, (%2) \n"//read from RAM
-					" vmovdqa %%ymm0, (%3) \n"//read from RAM
-					" vmovdqa %%ymm0, (%4) \n"//read from RAM
-					" vmovdqa %%ymm0, (%5) \n"//read from RAM
-					" vmovdqa %%ymm0, (%6) \n"//read from RAM
-					" vmovdqa %%ymm0, (%7) \n"//read from RAM
+					" clflush (%0) \n" 
+					" clflush (%1) \n"
+					" clflush (%2) \n"
+					" clflush (%3) \n"
+					" clflush (%4) \n"
+					" clflush (%5) \n"
+					" clflush (%6) \n"
+					" clflush (%7) \n"
+					" vmovdqa %%ymm0, (%0) \n" // write data to memory
+					" vmovdqa %%ymm0, (%1) \n"
+					" vmovdqa %%ymm0, (%2) \n"
+					" vmovdqa %%ymm0, (%3) \n"
+					" vmovdqa %%ymm0, (%4) \n"
+					" vmovdqa %%ymm0, (%5) \n"
+					" vmovdqa %%ymm0, (%6) \n"
+					" vmovdqa %%ymm0, (%7) \n"
 					: 
 					: "r" (addr[0]), "r" (addr[1]), "r" (addr[2]), "r" (addr[3]), "r" (addr[4]), "r" (addr[5]), "r" (addr[6]), "r" (addr[7])
 					: "%ymm0", "%ymm1", "%ymm2", "%ymm3");
@@ -131,56 +90,50 @@ for (rdLen=0;rdLen<arrayLen;rdLen++) {
 				//gettimeofday(&tv_begin,NULL);
 				for (val=1;val<sbDur[1];val++) {
 					asm volatile(
-					" clflush (%0) \n"//clear reg
-					" clflush (%1) \n"//clear reg
-					" clflush (%2) \n"//clear reg
-					" clflush (%3) \n"//clear reg
-					" clflush (%4) \n"//clear reg
-					" clflush (%5) \n"//clear reg
-					" clflush (%6) \n"//clear reg
-					" clflush (%7) \n"//clear reg
-					" vmovdqa %%ymm0, (%0) \n"//read from RAM
-					" vmovdqa %%ymm0, (%1) \n"//read from RAM
-					" vmovdqa %%ymm0, (%2) \n"//read from RAM
-					" vmovdqa %%ymm0, (%3) \n"//read from RAM
-					" vmovdqa %%ymm0, (%4) \n"//read from RAM
-					" vmovdqa %%ymm0, (%5) \n"//read from RAM
-					" vmovdqa %%ymm0, (%6) \n"//read from RAM
-					" vmovdqa %%ymm0, (%7) \n"//read from RAM
-					" mfence \n"
+					" clflush (%0) \n"
+					" clflush (%1) \n"
+					" clflush (%2) \n"
+					" clflush (%3) \n"
+					" clflush (%4) \n"
+					" clflush (%5) \n"
+					" clflush (%6) \n"
+					" clflush (%7) \n"
+					" vmovdqa %%ymm0, (%0) \n" // write data to memory
+					" vmovdqa %%ymm0, (%1) \n"
+					" vmovdqa %%ymm0, (%2) \n"
+					" vmovdqa %%ymm0, (%3) \n"
+					" vmovdqa %%ymm0, (%4) \n"
+					" vmovdqa %%ymm0, (%5) \n"
+					" vmovdqa %%ymm0, (%6) \n"
+					" vmovdqa %%ymm0, (%7) \n"
+					" mfence \n" // idle
 					" mfence \n"
 					" mfence \n"
 					:		
 					: "r" (addr[0]), "r" (addr[1]), "r" (addr[2]), "r" (addr[3]), "r" (addr[4]), "r" (addr[5]), "r" (addr[6]), "r" (addr[7])
 					: "%ymm0", "%ymm1", "%ymm2", "%ymm3");
-					//gettimeofday(&tv_end,NULL);
-					//dur=tv_end.tv_usec-tv_begin.tv_usec;
-					//if (dur>500000)
-						//break;
 				}
-				//printf("%lld\n",val);
-				//printf("S:%ld, E:%ld, dur:%ld\n",tv_begin.tv_usec,tv_end.tv_usec,dur);
 			break;
 
 			case 3:
 				for (val=1;val<sbDur[2];val++) {
 					asm volatile(
-					" clflush (%0) \n"//clear reg
-					" clflush (%1) \n"//clear reg
-					" clflush (%2) \n"//clear reg
-					" clflush (%3) \n"//clear reg
-					" clflush (%4) \n"//clear reg
-					" clflush (%5) \n"//clear reg
-					" clflush (%6) \n"//clear reg
-					" clflush (%7) \n"//clear reg
-					" vmovdqa %%ymm0, (%0) \n"//read from RAM
-					" vmovdqa %%ymm0, (%1) \n"//read from RAM
-					" vmovdqa %%ymm0, (%2) \n"//read from RAM
-					" vmovdqa %%ymm0, (%3) \n"//read from RAM
-					" vmovdqa %%ymm0, (%4) \n"//read from RAM
-					" vmovdqa %%ymm0, (%5) \n"//read from RAM
-					" vmovdqa %%ymm0, (%6) \n"//read from RAM
-					" vmovdqa %%ymm0, (%7) \n"//read from RAM
+					" clflush (%0) \n"
+					" clflush (%1) \n"
+					" clflush (%2) \n"
+					" clflush (%3) \n"
+					" clflush (%4) \n"
+					" clflush (%5) \n"
+					" clflush (%6) \n"
+					" clflush (%7) \n"
+					" vmovdqa %%ymm0, (%0) \n"
+					" vmovdqa %%ymm0, (%1) \n"
+					" vmovdqa %%ymm0, (%2) \n"
+					" vmovdqa %%ymm0, (%3) \n"
+					" vmovdqa %%ymm0, (%4) \n"
+					" vmovdqa %%ymm0, (%5) \n"
+					" vmovdqa %%ymm0, (%6) \n"
+					" vmovdqa %%ymm0, (%7) \n"
 					" mfence \n"
 					" mfence \n"
 					" mfence \n"
@@ -197,22 +150,22 @@ for (rdLen=0;rdLen<arrayLen;rdLen++) {
 			case 4:
 				for (val=1;val<sbDur[3];val++) {
 					asm volatile(
-					" clflush (%0) \n"//clear reg
-					" clflush (%1) \n"//clear reg
-					" clflush (%2) \n"//clear reg
-					" clflush (%3) \n"//clear reg
-					" clflush (%4) \n"//clear reg
-					" clflush (%5) \n"//clear reg
-					" clflush (%6) \n"//clear reg
-					" clflush (%7) \n"//clear reg
-					" vmovdqa %%ymm0, (%0) \n"//read from RAM
-					" vmovdqa %%ymm0, (%1) \n"//read from RAM
-					" vmovdqa %%ymm0, (%2) \n"//read from RAM
-					" vmovdqa %%ymm0, (%3) \n"//read from RAM
-					" vmovdqa %%ymm0, (%4) \n"//read from RAM
-					" vmovdqa %%ymm0, (%5) \n"//read from RAM
-					" vmovdqa %%ymm0, (%6) \n"//read from RAM
-					" vmovdqa %%ymm0, (%7) \n"//read from RAM
+					" clflush (%0) \n"
+					" clflush (%1) \n"
+					" clflush (%2) \n"
+					" clflush (%3) \n"
+					" clflush (%4) \n"
+					" clflush (%5) \n"
+					" clflush (%6) \n"
+					" clflush (%7) \n"
+					" vmovdqa %%ymm0, (%0) \n"
+					" vmovdqa %%ymm0, (%1) \n"
+					" vmovdqa %%ymm0, (%2) \n"
+					" vmovdqa %%ymm0, (%3) \n"
+					" vmovdqa %%ymm0, (%4) \n"
+					" vmovdqa %%ymm0, (%5) \n"
+					" vmovdqa %%ymm0, (%6) \n"
+					" vmovdqa %%ymm0, (%7) \n"
 					" mfence \n"//11
 					" mfence \n"
 					" mfence \n"
@@ -233,22 +186,22 @@ for (rdLen=0;rdLen<arrayLen;rdLen++) {
 			case 5:
 				for (val=1;val<sbDur[4];val++) {
 					asm volatile(
-					" clflush (%0) \n"//clear reg
-					" clflush (%1) \n"//clear reg
-					" clflush (%2) \n"//clear reg
-					" clflush (%3) \n"//clear reg
-					" clflush (%4) \n"//clear reg
-					" clflush (%5) \n"//clear reg
-					" clflush (%6) \n"//clear reg
-					" clflush (%7) \n"//clear reg
-					" vmovdqa %%ymm0, (%0) \n"//read from RAM
-					" vmovdqa %%ymm0, (%1) \n"//read from RAM
-					" vmovdqa %%ymm0, (%2) \n"//read from RAM
-					" vmovdqa %%ymm0, (%3) \n"//read from RAM
-					" vmovdqa %%ymm0, (%4) \n"//read from RAM
-					" vmovdqa %%ymm0, (%5) \n"//read from RAM
-					" vmovdqa %%ymm0, (%6) \n"//read from RAM
-					" vmovdqa %%ymm0, (%7) \n"//read from RAM
+					" clflush (%0) \n"
+					" clflush (%1) \n"
+					" clflush (%2) \n"
+					" clflush (%3) \n"
+					" clflush (%4) \n"
+					" clflush (%5) \n"
+					" clflush (%6) \n"
+					" clflush (%7) \n"
+					" vmovdqa %%ymm0, (%0) \n"
+					" vmovdqa %%ymm0, (%1) \n"
+					" vmovdqa %%ymm0, (%2) \n"
+					" vmovdqa %%ymm0, (%3) \n"
+					" vmovdqa %%ymm0, (%4) \n"
+					" vmovdqa %%ymm0, (%5) \n"
+					" vmovdqa %%ymm0, (%6) \n"
+					" vmovdqa %%ymm0, (%7) \n"
 					" mfence \n"//16
 					" mfence \n"
 					" mfence \n"
@@ -274,22 +227,22 @@ for (rdLen=0;rdLen<arrayLen;rdLen++) {
 			case 6:
 				for (val=1;val<sbDur[5];val++) {
 					asm volatile(
-					" clflush (%0) \n"//clear reg
-					" clflush (%1) \n"//clear reg
-					" clflush (%2) \n"//clear reg
-					" clflush (%3) \n"//clear reg
-					" clflush (%4) \n"//clear reg
-					" clflush (%5) \n"//clear reg
-					" clflush (%6) \n"//clear reg
-					" clflush (%7) \n"//clear reg
-					" vmovdqa %%ymm0, (%0) \n"//read from RAM
-					" vmovdqa %%ymm0, (%1) \n"//read from RAM
-					" vmovdqa %%ymm0, (%2) \n"//read from RAM
-					" vmovdqa %%ymm0, (%3) \n"//read from RAM
-					" vmovdqa %%ymm0, (%4) \n"//read from RAM
-					" vmovdqa %%ymm0, (%5) \n"//read from RAM
-					" vmovdqa %%ymm0, (%6) \n"//read from RAM
-					" vmovdqa %%ymm0, (%7) \n"//read from RAM
+					" clflush (%0) \n"
+					" clflush (%1) \n"
+					" clflush (%2) \n"
+					" clflush (%3) \n"
+					" clflush (%4) \n"
+					" clflush (%5) \n"
+					" clflush (%6) \n"
+					" clflush (%7) \n"
+					" vmovdqa %%ymm0, (%0) \n"
+					" vmovdqa %%ymm0, (%1) \n"
+					" vmovdqa %%ymm0, (%2) \n"
+					" vmovdqa %%ymm0, (%3) \n"
+					" vmovdqa %%ymm0, (%4) \n"
+					" vmovdqa %%ymm0, (%5) \n"
+					" vmovdqa %%ymm0, (%6) \n"
+					" vmovdqa %%ymm0, (%7) \n"
 					" mfence \n"//23
 					" mfence \n"
 					" mfence \n"
@@ -322,22 +275,22 @@ for (rdLen=0;rdLen<arrayLen;rdLen++) {
 			case 7:
 				for (val=1;val<sbDur[6];val++) {
 					asm volatile(
-					" clflush (%0) \n"//clear reg
-					" clflush (%1) \n"//clear reg
-					" clflush (%2) \n"//clear reg
-					" clflush (%3) \n"//clear reg
-					" clflush (%4) \n"//clear reg
-					" clflush (%5) \n"//clear reg
-					" clflush (%6) \n"//clear reg
-					" clflush (%7) \n"//clear reg
-					" vmovdqa %%ymm0, (%0) \n"//read from RAM
-					" vmovdqa %%ymm0, (%1) \n"//read from RAM
-					" vmovdqa %%ymm0, (%2) \n"//read from RAM
-					" vmovdqa %%ymm0, (%3) \n"//read from RAM
-					" vmovdqa %%ymm0, (%4) \n"//read from RAM
-					" vmovdqa %%ymm0, (%5) \n"//read from RAM
-					" vmovdqa %%ymm0, (%6) \n"//read from RAM
-					" vmovdqa %%ymm0, (%7) \n"//read from RAM
+					" clflush (%0) \n"
+					" clflush (%1) \n"
+					" clflush (%2) \n"
+					" clflush (%3) \n"
+					" clflush (%4) \n"
+					" clflush (%5) \n"
+					" clflush (%6) \n"
+					" clflush (%7) \n"
+					" vmovdqa %%ymm0, (%0) \n"
+					" vmovdqa %%ymm0, (%1) \n"
+					" vmovdqa %%ymm0, (%2) \n"
+					" vmovdqa %%ymm0, (%3) \n"
+					" vmovdqa %%ymm0, (%4) \n"
+					" vmovdqa %%ymm0, (%5) \n"
+					" vmovdqa %%ymm0, (%6) \n"
+					" vmovdqa %%ymm0, (%7) \n"
 					" mfence \n"//33
 					" mfence \n"
 					" mfence \n"
@@ -381,22 +334,22 @@ for (rdLen=0;rdLen<arrayLen;rdLen++) {
 			case 8:
 				for (val=1;val<sbDur[7];val++) {
 					asm volatile(		
-					" clflush (%0) \n"//clear reg
-					" clflush (%1) \n"//clear reg
-					" clflush (%2) \n"//clear reg
-					" clflush (%3) \n"//clear reg
-					" clflush (%4) \n"//clear reg
-					" clflush (%5) \n"//clear reg
-					" clflush (%6) \n"//clear reg
-					" clflush (%7) \n"//clear reg
-					" vmovdqa %%ymm0, (%0) \n"//read from RAM
-					" vmovdqa %%ymm0, (%1) \n"//read from RAM
-					" vmovdqa %%ymm0, (%2) \n"//read from RAM
-					" vmovdqa %%ymm0, (%3) \n"//read from RAM
-					" vmovdqa %%ymm0, (%4) \n"//read from RAM
-					" vmovdqa %%ymm0, (%5) \n"//read from RAM
-					" vmovdqa %%ymm0, (%6) \n"//read from RAM
-					" vmovdqa %%ymm0, (%7) \n"//read from RAM
+					" clflush (%0) \n"
+					" clflush (%1) \n"
+					" clflush (%2) \n"
+					" clflush (%3) \n"
+					" clflush (%4) \n"
+					" clflush (%5) \n"
+					" clflush (%6) \n"
+					" clflush (%7) \n"
+					" vmovdqa %%ymm0, (%0) \n"
+					" vmovdqa %%ymm0, (%1) \n"
+					" vmovdqa %%ymm0, (%2) \n"
+					" vmovdqa %%ymm0, (%3) \n"
+					" vmovdqa %%ymm0, (%4) \n"
+					" vmovdqa %%ymm0, (%5) \n"
+					" vmovdqa %%ymm0, (%6) \n"
+					" vmovdqa %%ymm0, (%7) \n"
 					" mfence \n"//46
 					" mfence \n"
 					" mfence \n"
@@ -455,6 +408,8 @@ for (rdLen=0;rdLen<arrayLen;rdLen++) {
 
 			break;
 		}
+		// if a chip transmission has been completed, wait for a certain period before transmitting the next chip.
+		// Due to computer instability causing slight variations in the time required to transmit each chip, we wait for a period at the end of each chip to ensure consistent signal duration.
 		gettimeofday( &curTime,NULL );
 		endTime=1000000*curTime.tv_sec+curTime.tv_usec;
 		if ((endTime-startTime)>waitus){
@@ -471,6 +426,7 @@ for (rdLen=0;rdLen<arrayLen;rdLen++) {
 
 void main(){
 	int i,j;
+	// construct the data to be sent
 	data[0]=0;
 	data[dataLen-1]=0;
 	for (i=1;i<=dataLen-2;i=i+sizeof(chip)/sizeof(chip[0]))
@@ -486,6 +442,8 @@ void main(){
 
 	}
 	else if (cnt==2){
+		// specify the number of memory write operations needed to transmit each of the 8 different chips.
+		// repeating different numbers of times ensures that the time taken to transmit each different chip is roughly the same.
 		SBDUR=8000;//120=1024
 		sbDur[0]=(int)2290;//2550
 		sbDur[1]=(int)2090;//2400
@@ -495,8 +453,6 @@ void main(){
 		sbDur[5]=(int)1420;//1650
 		sbDur[6]=(int)1200;//1450
 		sbDur[7]=(int)1080;//1250
-		waitus=1000;
-		sleepus=150;
 	}
 	else if (cnt==3)
 		exit(0);
@@ -505,13 +461,12 @@ void main(){
 		goto input;
 	}
 
-	//server();
-	//usleep (sleepus);
+	
 	gettimeofday(&curTime,NULL);
 	startTime=1000000 * curTime.tv_sec + curTime.tv_usec;
-	//printf("startTime:%ld\n",startTime);
-	WriteRAM(data,dataLen);
+	WriteRAM(data,dataLen);// transmit payload (i.e., testing data)
 
+	// specify the number of memory write operations needed to transmit the header of the packet (i.e., training data).
 	SBDUR=1070000;
 	sbDur[0]=(int)255000;
 	sbDur[1]=(int)240000;
@@ -523,8 +478,6 @@ void main(){
 	sbDur[7]=(int)125000;
 	waitus=100000;
 	
-	WriteRAM(head,headLen);
+	WriteRAM(head,headLen);// transmit packet header (i.e., training data)
 
-
-	//printf("\nfinish sending");
 }
