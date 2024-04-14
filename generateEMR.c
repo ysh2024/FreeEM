85527ff0981f7ef65839a8845c732edfcb0b9808
ysh2024 <165976269+ysh2024@users.noreply.github.com>
2024-04-13 20:40:48 -0400

    Update generateEMR.c

--- generateEMR.c
+++ generateEMR.c
@@ -20,22 +20,10 @@
 __m512i addr[8][4096];
 long long val;
 int cnt;
-//4 or 3 senders, use chip[130]; 2 senders, use data[258]; 130=0+128+0; 258=0+256+0; 0 is used as guard.
 
 int head[32]={1,2,3,4,5,6,7,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-
 int data[2562];
-
 int chip[128]={1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,1,3,3,3,3,3,3,3,1,4,4,4,4,4,4,4,1,5,5,5,5,5,5,5,1,6,6,6,6,6,6,6,1,7,7,7,7,7,7,7,1,8,8,8,8,8,8,8,2,1,2,3,4,5,6,7,2,2,1,4,3,6,5,8,2,3,4,1,2,7,8,5,2,4,3,2,1,8,7,6,2,5,6,7,8,1,2,3,2,6,5,8,7,2,1,4,2,7,8,5,6,3,4,1,2,8,7,6,5,4,3,2};
-//int chip[128]={3,1,3,5,7,4,2,8,3,2,4,6,8,3,1,7,3,3,1,7,5,2,4,6,3,4,2,8,6,1,3,5,3,5,7,1,3,8,6,4,3,6,8,2,4,7,5,3,3,7,5,3,1,6,8,2,3,8,6,4,2,5,7,1,4,1,4,7,6,8,5,2,4,2,3,8,5,7,6,1,4,3,2,5,8,6,7,4,4,4,1,6,7,5,8,3,4,5,8,3,2,4,1,6,4,6,7,4,1,3,2,5,4,7,6,1,4,2,3,8,4,8,5,2,3,1,4,7};
-//int chip[128]={5,1,5,4,8,7,3,6,5,2,6,3,7,8,4,5,5,3,7,2,6,5,1,8,5,4,8,1,5,6,2,7,5,5,1,8,4,3,7,2,5,6,2,7,3,4,8,1,5,7,3,6,2,1,5,4,5,8,4,5,1,2,6,3,6,1,6,2,5,3,8,4,6,2,5,1,6,4,7,3,6,3,8,4,7,1,6,2,6,4,7,3,8,2,5,1,6,5,2,6,1,7,4,8,6,6,1,5,2,8,3,7,6,7,4,8,3,5,2,6,6,8,3,7,4,6,1,5};
-//int chip[128]={7,1,7,8,2,6,4,3,7,2,8,7,1,5,3,4,7,3,5,6,4,8,2,1,7,4,6,5,3,7,1,2,7,5,3,4,6,2,8,7,7,6,4,3,5,1,7,8,7,7,1,2,8,4,6,5,7,8,2,1,7,3,5,6,8,1,8,6,3,2,7,5,8,2,7,5,4,1,8,6,8,3,6,8,1,4,5,7,8,4,5,7,2,3,6,8,8,5,4,2,7,6,3,1,8,6,3,1,8,5,4,2,8,7,2,4,5,8,1,3,8,8,1,3,6,7,2,4};
-
-//int chip[256]={1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,1,3,3,3,3,3,3,3,1,4,4,4,4,4,4,4,1,5,5,5,5,5,5,5,1,6,6,6,6,6,6,6,1,7,7,7,7,7,7,7,1,8,8,8,8,8,8,8,2,1,2,3,4,5,6,7,2,2,1,4,3,6,5,8,2,3,4,1,2,7,8,5,2,4,3,2,1,8,7,6,2,5,6,7,8,1,2,3,2,6,5,8,7,2,1,4,2,7,8,5,6,3,4,1,2,8,7,6,5,4,3,2,3,1,3,5,7,4,2,8,3,2,4,6,8,3,1,7,3,3,1,7,5,2,4,6,3,4,2,8,6,1,3,5,3,5,7,1,3,8,6,4,3,6,8,2,4,7,5,3,3,7,5,3,1,6,8,2,3,8,6,4,2,5,7,1,4,1,4,7,6,8,5,2,4,2,3,8,5,7,6,1,4,3,2,5,8,6,7,4,4,4,1,6,7,5,8,3,4,5,8,3,2,4,1,6,4,6,7,4,1,3,2,5,4,7,6,1,4,2,3,8,4,8,5,2,3,1,4,7};
-//int chip[256]={5,1,5,4,8,7,3,6,5,2,6,3,7,8,4,5,5,3,7,2,6,5,1,8,5,4,8,1,5,6,2,7,5,5,1,8,4,3,7,2,5,6,2,7,3,4,8,1,5,7,3,6,2,1,5,4,5,8,4,5,1,2,6,3,6,1,6,2,5,3,8,4,6,2,5,1,6,4,7,3,6,3,8,4,7,1,6,2,6,4,7,3,8,2,5,1,6,5,2,6,1,7,4,8,6,6,1,5,2,8,3,7,6,7,4,8,3,5,2,6,6,8,3,7,4,6,1,5,7,1,7,8,2,6,4,3,7,2,8,7,1,5,3,4,7,3,5,6,4,8,2,1,7,4,6,5,3,7,1,2,7,5,3,4,6,2,8,7,7,6,4,3,5,1,7,8,7,7,1,2,8,4,6,5,7,8,2,1,7,3,5,6,8,1,8,6,3,2,7,5,8,2,7,5,4,1,8,6,8,3,6,8,1,4,5,7,8,4,5,7,2,3,6,8,8,5,4,2,7,6,3,1,8,6,3,1,8,5,4,2,8,7,2,4,5,8,1,3,8,8,1,3,6,7,2,4};
-
-//int chip[512]={1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,1,3,3,3,3,3,3,3,1,4,4,4,4,4,4,4,1,5,5,5,5,5,5,5,1,6,6,6,6,6,6,6,1,7,7,7,7,7,7,7,1,8,8,8,8,8,8,8,2,1,2,3,4,5,6,7,2,2,1,4,3,6,5,8,2,3,4,1,2,7,8,5,2,4,3,2,1,8,7,6,2,5,6,7,8,1,2,3,2,6,5,8,7,2,1,4,2,7,8,5,6,3,4,1,2,8,7,6,5,4,3,2,3,1,3,5,7,4,2,8,3,2,4,6,8,3,1,7,3,3,1,7,5,2,4,6,3,4,2,8,6,1,3,5,3,5,7,1,3,8,6,4,3,6,8,2,4,7,5,3,3,7,5,3,1,6,8,2,3,8,6,4,2,5,7,1,4,1,4,7,6,8,5,2,4,2,3,8,5,7,6,1,4,3,2,5,8,6,7,4,4,4,1,6,7,5,8,3,4,5,8,3,2,4,1,6,4,6,7,4,1,3,2,5,4,7,6,1,4,2,3,8,4,8,5,2,3,1,4,7,5,1,5,4,8,7,3,6,5,2,6,3,7,8,4,5,5,3,7,2,6,5,1,8,5,4,8,1,5,6,2,7,5,5,1,8,4,3,7,2,5,6,2,7,3,4,8,1,5,7,3,6,2,1,5,4,5,8,4,5,1,2,6,3,6,1,6,2,5,3,8,4,6,2,5,1,6,4,7,3,6,3,8,4,7,1,6,2,6,4,7,3,8,2,5,1,6,5,2,6,1,7,4,8,6,6,1,5,2,8,3,7,6,7,4,8,3,5,2,6,6,8,3,7,4,6,1,5,7,1,7,8,2,6,4,3,7,2,8,7,1,5,3,4,7,3,5,6,4,8,2,1,7,4,6,5,3,7,1,2,7,5,3,4,6,2,8,7,7,6,4,3,5,1,7,8,7,7,1,2,8,4,6,5,7,8,2,1,7,3,5,6,8,1,8,6,3,2,7,5,8,2,7,5,4,1,8,6,8,3,6,8,1,4,5,7,8,4,5,7,2,3,6,8,8,5,4,2,7,6,3,1,8,6,3,1,8,5,4,2,8,7,2,4,5,8,1,3,8,8,1,3,6,7,2,4};
-
 
 
 struct timeval curTime;
@@ -79,96 +67,10 @@ int server(void)
 {
 	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
 	int cnt=1, fourClient[4];
-	/*char content[20];
-	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr; // connector's address information
-	socklen_t sin_size;
-	struct sigaction sa;
-	int yes=1;
-	char s[INET6_ADDRSTRLEN];
-	int rv;
 
-	
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; // use my IP
-
-	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-
-	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("server: socket");
-			continue;
-		}
-
-		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-				sizeof(int)) == -1) {
-			perror("setsockopt");
-			exit(1);
-		}
-
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("server: bind");
-			continue;
-		}
-
-		break;
-	}
-
-	freeaddrinfo(servinfo); // all done with this structure
-
-	if (p == NULL)  {
-		fprintf(stderr, "server: failed to bind\n");
-		exit(1);
-	}
-
-	if (listen(sockfd, BACKLOG) == -1) {
-		perror("listen");
-		exit(1);
-	}
-
-	sa.sa_handler = sigchld_handler; // reap all dead processes
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
-
-	printf("server: waiting for connections...\n");*/
-
-	while(1) {  // main accept() loop
-		/*sin_size = sizeof their_addr;
-		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (new_fd == -1) {
-			perror("accept");
-			continue;
-		}
-
-		inet_ntop(their_addr.ss_family,
-			get_in_addr((struct sockaddr *)&their_addr),
-			s, sizeof s);
-		printf("server: got connection from %s\n", s);
-		printf("new_fd: %d\n", new_fd);
-		fourClient[cnt]=new_fd;
-		cnt++;*/
+	while(1) {
 		if (cnt==1){
-			//gettimeofday( &curTime,NULL );
-			//sendTime =curTime.tv_sec;
-			//sprintf(content,"%ld",sendTime);
 			send(fourClient[0], "begin", 5, 0);
-			//send(fourClient[1], "begin", 5, 0);
-			//send(fourClient[2], "begin", 5, 0);
-			//close(fourClient[0]);
-			//close(fourClient[1]);
-			//close(fourClient[2]);
 			break;
 		}
 	}
@@ -574,8 +476,6 @@ void main(){
 	for (i=1;i<=dataLen-2;i=i+sizeof(chip)/sizeof(chip[0]))
 		for (j=i;j<i+sizeof(chip)/sizeof(chip[0]);j++)
 			data[j]=chip[j-i];
-	/*for (i=0;i<dataLen;i=i+1)
-		printf("%d,",data[i]);*/
 
 	printf("data length:%lld\n",dataLen);
 
@@ -586,7 +486,6 @@ void main(){
 
 	}
 	else if (cnt==2){
-
 		SBDUR=8000;//120=1024
 		sbDur[0]=(int)2290;//2550
 		sbDur[1]=(int)2090;//2400
@@ -605,16 +504,9 @@ void main(){
 		printf("input error!\n");
 		goto input;
 	}
-	/*gettimeofday(&tv_begin,NULL);
-	thres=tv_begin.tv_usec/100000*100000+100000;
-	printf("thres:%ld\n",thres);
-	gettimeofday(&tv_end,NULL);
-	while (tv_end.tv_usec<thres) {
-		gettimeofday(&tv_end,NULL);
-		//printf("cur:%ld\n",tv_end.tv_usec);
-	}*/
-	server();
-	usleep (sleepus);
+
+	//server();
+	//usleep (sleepus);
 	gettimeofday(&curTime,NULL);
 	startTime=1000000 * curTime.tv_sec + curTime.tv_usec;
 	//printf("startTime:%ld\n",startTime);
